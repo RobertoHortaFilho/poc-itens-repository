897ab2cfceaa9c44471a3b3d3f4c8c02f7f934b6
RobertoHortaFilho <robertohortafilho@gmail.com>
2025-03-02 05:24:00 -0300

    reload

--- components/dialog/index.tsx
+++ components/dialog/index.tsx
@@ -12,7 +12,7 @@ export default function Dialog() {
       className={`${visible ? "visible" : "hidden"}
 			 bg-green-400 h-12 flex justify-between items-center rounded-b-xl`}
     >
-      <p className={`text-gray-900 pl-4`}>Aviso personalizado... WIP</p>
+      <p className={`text-gray-900 pl-4`}>Aviso personalizado... WIP!</p>
       <button className="m-0 mr-4 p-0" onClick={onCloseDialog}>
         <p className="text-red-600 hover:scale-110 transform transition-transform duration-150">
           X
